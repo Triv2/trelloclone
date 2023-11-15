7d48847c4bd0f7495946b57ff6c76e1be2e35818
Trivial-lore <triviallore@gmail.com>
2023-11-15 14:28:29 -0600

    added new font and shadcn button, page updates

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@/components/ui/button";
 import { Medal } from "lucide-react";
+import Link from "next/link";
 
 const MarketingPage = () => {
   return (
@@ -11,7 +13,19 @@ const MarketingPage = () => {
         <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">
           Taskify helps teams move
         </h1>
+        <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
+          work forward.
+        </div>
       </div>
+      <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
+        Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is
+        unique - accomplish it all with Taskify.
+      </div>
+      <Button className="mt-6" size="lg" asChild>
+        <Link href="/">
+        Get Taskify for free
+        </Link>
+      </Button>
     </div>
   )
 }
